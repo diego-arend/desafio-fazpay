8f87f10ffaaba29a279793f67a887e470256fafd
Diego Arend <d.arend@paylivre.com>
2024-04-03 15:29:28 -0300

    - Aumento de cobertura test Login

--- src/test/pages/public/loginPage.test.tsx
+++ src/test/pages/public/loginPage.test.tsx
@@ -1,26 +1,18 @@
-import { cleanup, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import { cleanup, fireEvent, screen, waitFor } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import LoginPage from "../../../pages/public/loginPage";
 import GeneralRenderContexts from "../../GeneralProviderContexts";
 import { ROUTES } from "../../../constants/routes/routes";
+import LoginRequest from "../../../api/requests/loginRequest";
+
+//Configs teste
+jest.mock("../../../api/requests/loginRequest.ts");
 
 describe("Login Page", () => {
   beforeEach(() => {
     jest.resetAllMocks();
     cleanup();
-  });
-
-  test("Login Page login", async () => {
-    act(() => {
-      GeneralRenderContexts(<LoginPage />);
-    });
-
-    const emailInput = screen.getByTestId("login-email");
-    const passwordInput = screen.getByTestId("login-password");
-
-    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
-    fireEvent.change(passwordInput, { target: { value: "password" } });
   });
 
   test("Login Page render and change route link", async () => {
@@ -53,8 +45,48 @@ describe("Login Page", () => {
 
     fireEvent.click(registerLink);
 
-    waitFor(() => {
-      expect(window.location.pathname).toBe(ROUTES.REGISTER);
+    act(() => {
+      waitFor(() => {
+        expect(window.location.pathname).toBe(ROUTES.REGISTER);
+      });
     });
+  });
+
+  test("Login Page login", async () => {
+    act(() => {
+      GeneralRenderContexts(<LoginPage />);
+    });
+
+    (LoginRequest as jest.Mock).mockResolvedValue({
+      data: {
+        token: "token",
+        user: {
+          id: 1,
+          name: "usuario",
+          email: "test@example.com",
+          lastName: "de teste",
+        },
+      },
+    });
+
+    const emailInput = screen.getByTestId("login-email");
+    const passwordInput = screen.getByTestId("login-password");
+    const submitButton = screen.getByRole("button", {
+      name: /acessar/i,
+    });
+
+    fireEvent.change(emailInput, { target: { value: "test@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
+    fireEvent.click(submitButton);
+
+    await act(async () => {
+      jest.advanceTimersByTime(200);
+
+      await waitFor(() => {
+        expect(window.location.pathname).toBe(ROUTES.LIST);
+      });
+    });
+
+    expect(window.location.pathname).toBe(ROUTES.LIST);
   });
 });
